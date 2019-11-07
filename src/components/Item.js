8f19b90d21c4ffa91d3fa70a164fb9fa2da8cf17
MKortmann <companymvms@gmail.com>
2019-11-07 18:25:31 +0100

    add: Item + styled deleted

--- src/components/Item.js
+++ src/components/Item.js
@@ -1,9 +1,19 @@
 import React from "react";
+import Grid from '@material-ui/core/Grid';
+import Typography from '@material-ui/core/Typography';
+import DeleteOutlinedIcon from '@material-ui/icons/DeleteOutlined';
 
 
 const item = (props) => {
   return (
-    <h4 onClick={props.clicked}>{props.label}</h4>
+    <Grid container>
+    <Grid item xs={6}>
+      <Typography>{props.label}</Typography>
+    </Grid>
+    <Grid item xs={6}>
+      <DeleteOutlinedIcon onClick={props.clicked}/>
+    </Grid>
+    </Grid>
   )
 }
 
