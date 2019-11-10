2110066c582d7363caa12a47e5914f1a2179d07f
MKortmann <companymvms@gmail.com>
2019-11-10 18:31:47 +0100

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/MKortmann/Health-Tracker/precache-manifest.fd972abde0e1fb8fbd117a40b78c303b.js"
+  "/MKortmann/Notes-Tracker/precache-manifest.5f053a418960d82abcaf90b98ed5306e.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/MKortmann/Health-Tracker/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/MKortmann/Notes-Tracker/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
